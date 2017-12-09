165cbb6a96e2995701232ad2d4814faae340573a
Thor1568 <33670825+Thor1568@users.noreply.github.com>
2017-12-08 23:40:37 -0500

    Update MECH Trinity.py

--- MECH Trinity.py
+++ MECH Trinity.py
@@ -5,10 +5,10 @@ from time import sleep
 import pygame
 import os
 import sys
-exec("/Users/UP2-Desk/Documents/Programming/Python/Games/Pygame_games/MECH_Trinity/config/constants.py")
 
 g_dir = os.getcwd()
 cfig_dir = os.path.normpath(g_dir+"/config/ ")
 cfig_dir= cfig_dir.strip()
 img_dir = os.path.normpath(g_dir+"/images/ ")
 img_dir= img_dir.strip()
+exec("g_dir+/MECH_Trinity/config/constants.py")
