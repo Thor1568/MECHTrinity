ae574ab7a5d2e0447de25ef1267952d753407dff
Thor1568 <n0002k2@gmail.com>
2017-12-09 18:35:34 -0500

    Tile engine completion?

--- config/map_loader.py
+++ config/map_loader.py
@@ -4,6 +4,8 @@ import os
 from pytmx.util_pygame import load_pygame
 from time import sleep
 
+#Partially my code but some is edited from a yt video of: https://www.youtube.com/watch?v=QIXyj3WeyZM&list=PLsk-HSGFjnaGQq7ybM8Lgkh5EMxUWPm2i&index=12
+#Also from https://www.youtube.com/watch?v=Mr5l4U9S4kI
 class TileMap:
     def __init__(self, file):
         tm = load_pygame(file, pixelalpha=True)
@@ -11,7 +13,7 @@ class TileMap:
         self.height = tm.height * tm.tileheight
         self.tmx_map = tm
 
-    def render(self, display, surface):
+    def render(self, display, surface, tile_group):
         """Renders the tilemap upon the specified surface"""
         tID = self.tmx_map.get_tile_image_by_gid
         for layer in self.tmx_map.visible_layers:
@@ -19,33 +21,42 @@ class TileMap:
                 for x, y, gid in layer:
                     tile = tID(gid)
                     if tile:
-                        temp_tile = tile_sprite((x*self.tmx_map.tilewidth, y*self.tmx_map.tileheight), )
-
+                        temp_tile = tile_sprite((x*self.tmx_map.tilewidth, y*self.tmx_map.tileheight), tile)
+                        tile_group.add(temp_tile)
 class tile_sprite(pygame.sprite.Sprite):
     def __init__(self, pos, tile):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(tile)
+        self.image = tile
         self.rect = self.image.get_rect()
         self.place = pos
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        self.type == tType
 
 def main():
+    tiles_group = pygame.sprite.LayeredUpdates()
     temp_surf = pygame.Surface((800, 600))
     path = "C:/Users/UP2-Desk/Documents/Programming/Python/Games/Pygame_games/MECH_Trinity/config/maps/testmap.tmx"
     path = os.path.normpath(path)
     mysurf = pygame.display.set_mode((800, 600), 0, 32)
     mysurf.fill((0,0,0))
     tmx_thing = TileMap(path)
-    tmx_thing.render(temp_surf, mysurf)
+    tmx_thing.render(temp_surf, mysurf, tiles_group)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
                     exit()
                 if event.key == pygame.K_UP:
                     print("UP ARROW")
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        tiles_group.draw(mysurf)
+        print(tiles_group)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
+
+print(tiles_group)
--- config/tile_loader2.py
+++ config/tile_loader2.py
@@ -1,3 +1,32 @@
 #Map test with my own code
-from ptmx.util_pygame import load_pygame
-test_map = load_pygame(map_file)
+from pytmx.util_pygame import load_pygame
+import pytmx
+import pygame
+import os
+
+g_dir = os.getcwd()
+#map_dir = os.path.normpath(g_dir+"/config/maps/ ")
+#map_dir= map_dir.strip()
+cfig_dir = os.path.normpath(g_dir+"/config/ ")
+cfig_dir= cfig_dir.strip()
+#map_file = map_dir+"testmap.tmx"
+map_file = pygame.image.load(cfig_dir+"testmap.png")
+gameDisp = pygame.display.set_mode((800, 600), 0, 32)
+gameDisp.fill((0,0,0))
+#test_map = load_pygame(map_file)
+#for layer in test_map.visible_layers:
+#    for x, y, gid, in layer:
+#        tile = test_map.get_tile_image_by_gid(gid)
+#        gameDisp.blit(tile, (x * test_map.tilewidth, y * test_map.tileheight))
+gameDisp.blit(map_file, (0,0))
+pygame.display.flip()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                break
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
+print("yayuz")
