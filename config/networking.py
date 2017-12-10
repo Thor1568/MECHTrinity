e713f74986f9edc7b51cd6a755bd82840e37e4b6
Thor1568 <n0002k2@gmail.com>
2017-12-09 21:41:53 -0500

    added broken server module

--- config/networking.py
+++ config/networking.py
@@ -1,33 +1,91 @@
 import socket
+import threading
+
+#Variable to check who is using printing and keep it from being used twice at once
 
 #In progress networking classes
-port = 6573
 class Server:
     def __init__(self, timeout, max_users):
-        self = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.setdefaulttimeout(timeout)
-        self.bind((6573, ip))
-        self.listen(max_users)
+        socket.setdefaulttimeout(timeout)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = 6573
+        self.host_name = socket.gethostname()
+        self.ip = socket.gethostbyname(self.host_name)
+        self.sock.bind((self.ip, 6573))
+        self.sock.listen(max_users)
         self.addresses = []
+        self.conns = []
 
-    def run(self):
-        #Function that will be run and maintains connections between each Client
-        c, addr = self.accept()
-        self.addresses.add(addr)
+    def work(self):
+        #Function that will be run and maintains connections between each Client and is run by thread
+        while True:
+            try:
+                c, addr = self.sock.accept()
+                self.addresses.append(addr)
+                self.conns.append(c)
+            except:
+                pass
+
+
+    def recieve(self):
+        data = self.sock.recv(1024)
+        data = tuple(data.decode())
+        return data
+
+    def myInfo(self):
+        return (self.addresses, (self.host_name, self.ip, self.port))
+
+    def myConns(self):
+        return self.conns
+
 
 
 class Client:
-    def __init__(self, ip):
-        self = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #Two connection methods
-        self.connect((6573, ip))
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = 6573
+        self.host = socket.gethostname()
+        #Two connection methods self.connect((ip, port)) or self.create_connection((ip, port))
 
-    def run(self):
-        #Function that maintains a connection with the server class
+    def make_conn(self, ip):
+        self.ip_conn = ip
+        self.sock.connect((ip, self.port))
+
+    def myInfo(self):
+        return (self.host, self.port)
+
+    def work(self):
+        #Function that maintains a connection with the server class and is controlled by thread
         pass
 
+    def transmit(self, data):
+        data = str((data, self.host))
+        data = data.encode()
+        self.sock.send(data)
+
+    def recieve(self):
+        data = self.sock.recv(1024)
+        data = tuple(data.decode())
+        return data
+
+
 def main():
-    pass
+    gameServer = Server(5, 1)
+    hoi = "Hello Server!"
+    ip = "192.168.20.226"
+    print("Server made")
+    gServer_thread = threading.Thread(target=gameServer.work)
+    print(gameServer.myInfo())
+    gameClient = Client()
+    gameClient.make_conn("192.168.20.226")
+    print("connection established")
+    gClient_thread = threading.Thread(target=gameClient.transmit, args=hoi)
+    print(gameServer.myInfo())
+    gServer_thread.start()
+    print(gameServer.recieve())
+    gClient_thread.start()
+
+
 
 if __name__ == "__main__":
     main()
