abb497ef4f5b8cf9d5d4b466b8337640cf66cea7
Thor1568 <n0002k2@gmail.com>
2017-12-09 19:54:29 -0500

    changes yay

--- config/maps/testset1.tsx
+++ config/maps/testset1.tsx
@@ -1,13 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="MechTestset1" tilewidth="50" tileheight="50" tilecount="3" columns="0">
+<tileset name="MechTestset1" tilewidth="50" tileheight="50" tilecount="8" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="11">
+  <image width="50" height="50" source="../../images/tiles/desert_sand.png"/>
+ </tile>
+ <tile id="12">
+  <image width="50" height="50" source="../../images/tiles/dry dirt.png"/>
+ </tile>
+ <tile id="13">
   <image width="50" height="50" source="../../images/tiles/grass1.png"/>
  </tile>
- <tile id="1">
+ <tile id="14">
   <image width="50" height="50" source="../../images/tiles/grass2.png"/>
  </tile>
- <tile id="2">
+ <tile id="15">
+  <image width="50" height="50" source="../../images/tiles/loamy_soil.png"/>
+ </tile>
+ <tile id="16">
   <image width="50" height="50" source="../../images/tiles/metal_floor.png"/>
+ </tile>
+ <tile id="17">
+  <image width="50" height="50" source="../../images/tiles/reddish_sand.png"/>
+ </tile>
+ <tile id="18">
+  <image width="50" height="50" source="../../images/tiles/rocky dirt.png"/>
  </tile>
 </tileset>
